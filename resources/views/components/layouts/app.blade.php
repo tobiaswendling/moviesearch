70be9185cfae8d1c6ca27b6ca98aabbec7db8a68
Tobias Wendling <tobiaswendling@icloud.com>
2025-06-08 17:50:28 +0200

    chore: add flux

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,8 +10,10 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @fluxAppearance
 </head>
 <body>
     {{ $slot }}
+    @fluxScripts
 </body>
 </html>
