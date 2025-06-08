72894d18ba5e56c00824a7d68e8224382d9674fd
Tobias Wendling <tobiaswendling@icloud.com>
2025-06-08 17:45:09 +0200

    chore: add Livewire and basic layout component

---
+++ app/Livewire/Pages/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Illuminate\View\View;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render(): View
+    {
+        return view('livewire.pages.index');
+    }
+}
